58a3122980050fe94acb1e1da321cbe7cf9278e9
Ardonit Saliji <ardonit.1980@gmail.com>
2022-05-04 20:43:32 +0200

    append

--- mc.js
+++ mc.js
@@ -1,8 +1,37 @@
 let priceTeller = document.querySelector('#price')
-let x = document.getElementsByClassName('x')
-let extraItem = document.getElementsByClassName('extraItem')
-let extraThing = document.getElementById('extraThing')
-let itemQuantity = document.getElementById('itemQuantity')
+let x = document.querySelectorAll('.x')
+let extraItems = $('.extraItems')
+let extraItem = $('.extraItem')
+let extraThing = document.querySelector('select')
+let itemQuantity = $('#itemQuantity')
+let addButton = $('.add')
+
+let span = document.createElement('span')
+span.classList.add('x')
+
+let addExtraItem = document.createElement('p')
+let text = document.createTextNode(
+  extraThing.options[extraThing.selectedIndex].text
+)
+
+addExtraItem.append('Extra ' + text.textContent)
+addExtraItem.classList.add('extraItem')
+
+addButton.on('click', () => {
+  extraItem
+    .text(itemQuantity.val() + ' ' + 'Extra ' + text.textContent)
+    .append(span)
+  addExtraItem.append(span)
+  extraItems.append(addExtraItem)
+  return extraItem
+})
+
+$(span).click(function (e) {
+  $(e.target).parent().hide(500)
+})
+$('.x').click(function (e) {
+  $(e.target).parent().hide(500)
+})
 
 $('#quantity').on('keyup', function () {
   $(priceTeller).text('$' + this.value * 4.49)
@@ -14,13 +43,3 @@ $('#cancel').click(function () {
 $('#quarter').click(function () {
   $('.quarter-popup').show(500)
 })
-$('.extraItem .x').click(function (e) {
-  $(e.target).parent().hide(500)
-})
-
-$(function () {
-  $('.add').click(function () {
-    $(extraItem).text(itemQuantity + extraThing)
-  })
-})
-console.log(extraItem)
