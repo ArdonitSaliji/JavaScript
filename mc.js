72bc77b58a37873d2edb268cc155c0c346fc4a59
Ardonit Saliji <ardonit.1980@gmail.com>
2022-05-04 12:40:38 +0200

    1

--- mc.js
+++ mc.js
@@ -1,6 +1,8 @@
 let priceTeller = document.querySelector('#price')
 let x = document.getElementsByClassName('x')
 let extraItem = document.getElementsByClassName('extraItem')
+let extraThing = document.getElementById('extraThing')
+let itemQuantity = document.getElementById('itemQuantity')
 
 $('#quantity').on('keyup', function () {
   $(priceTeller).text('$' + this.value * 4.49)
@@ -15,3 +17,10 @@ $('#quarter').click(function () {
 $('.extraItem .x').click(function (e) {
   $(e.target).parent().hide(500)
 })
+
+$(function () {
+  $('.add').click(function () {
+    $(extraItem).text(itemQuantity + extraThing)
+  })
+})
+console.log(extraItem)
