a37b44e731b2ff7a3d23b9a43578b504111f5366
Ardonit Saliji <ardonit.1980@gmail.com>
2022-05-06 23:05:12 +0200

    1

--- mc.js
+++ mc.js
@@ -6,27 +6,26 @@ let extraThing = document.getElementById('extraThing')
 let itemQuantity = $('#itemQuantity')
 let quantity = $('#quantity')
 let addButton = $('.add')
-let number = 0.25
+
 let total = $('.total')
-let totalNumber = $('#totalN')
 
 $(function () {
   total.text('Total: ' + priceTeller.textContent)
 })
+let price = document.getElementById('price').innerHTML
+let priceSplit = price?.split('$')
 
-$('#quantity').on('keyup', function () {
-  $(priceTeller).text('$' + this.value * 4.49)
-  total.text('Total: ' + priceTeller.textContent)
-})
+// $('#quantity').on('keyup', function () {
+//   $(priceTeller).text('$' + this.value * parseFloat(priceSplit[1]))
+// })
+
 addButton.on('click', function () {
   let total = $('.total')
   let text = document.createTextNode(
     extraThing.options[extraThing.selectedIndex].text
   )
-
+  let number = 0.25 * itemQuantity.val() * quantity.val()
   let option = $('option:selected').val()
-  
-  
 
   let addExtraItem = $(`<p class='extraItem'><span class='x'></span></p>`)
 
@@ -34,68 +33,19 @@ addButton.on('click', function () {
     addExtraItem.prepend(
       itemQuantity.val() +
         ' Extra ' +
-        (text.textContent =
-          'Tomatoes&nbsp;&nbsp;' + ' +$' + number * itemQuantity.val())
+        (text.textContent = 'Tomatoes&nbsp;&nbsp;' + ' +$' + number)
     )
-    var elem = document.getElementById('tot').innerHTML;
-    var elements = elem?.split('$');
-    console.log(parseFloat(elements[1]))
-    total.text(
-      'Total: $' + (quantity.val() * parseFloat(elements[1]) + number * itemQuantity.val())
-    )
-  }
 
-  if (option == 2) {
-    addExtraItem.prepend(
-      itemQuantity.val() +
-        ' Extra ' +
-        (text.textContent =
-          'Prickles&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +
-          ' +$' +
-          (0.05 + number) * itemQuantity.val())
-    )
-    total.text(
-      'Total: $' +
-        (quantity.val() * 4.49 + (0.05 + number) * itemQuantity.val())
-    )
-  } else if (option == 3) {
-    addExtraItem.prepend(
-      itemQuantity.val() +
-        ' Extra ' +
-        (text.textContent =
-          'Cabbages&nbsp;' + ' +$' + (number + 0.1) * itemQuantity.val())
-    )
-    total.text(
-      'Total: $' + (quantity.val() * 4.49 + (0.1 + number) * itemQuantity.val())
-    )
-  } else if (option == 4) {
-    addExtraItem.prepend(
-      itemQuantity.val() +
-        ' Extra ' +
-        (text.textContent =
-          'Cheese&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +
-          ' +$' +
-          (number + 0.15) * itemQuantity.val())
-    )
-    total.text(
-      'Total: $' +
-        (quantity.val() * 4.49 + (0.15 + number) * itemQuantity.val())
-    )
-  } else if (option == 5) {
-    addExtraItem.prepend(
-      itemQuantity.val() +
-        ' Extra ' +
-        (text.textContent =
-          'Onions&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +
-          ' +$' +
-          (number - 0.05) * itemQuantity.val())
-    )
-    total.text(
-      'Total: $' +
-        (quantity.val() * 4.49 + (number - 0.05) * itemQuantity.val())
-    )
+    var elem = document.getElementById('tot').innerHTML
+    var elements = elem?.split('$')
+    if (quantity.val() > 1) {
+      total.text('Total: $' + (parseFloat(elements[1]) + number))
+    } else {
+      total.text('Total: $' + (parseFloat(elements[1]) + number))
+    }
+    console.log(elements[1])
+    extraItems.append(addExtraItem)
   }
-  extraItems.append(addExtraItem)
 
   $('span').click(function (e) {
     $(e.target).parent().hide(500)
