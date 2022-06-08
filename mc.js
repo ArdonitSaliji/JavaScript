a5f8e9cb178cddc515c8c632e21a519bb50fd0ac
Ardonit Saliji <ardonit.1980@gmail.com>
2022-06-09 01:05:24 +0200

    Bug fixed

--- mc.js
+++ mc.js
@@ -3,34 +3,44 @@ let extraItems = $('.extraItems');
 let extraItem = $('.extraItem');
 let itemQuantity = $('#itemQuantity');
 let quantity = $('#quantity');
-
+let extraItemText = $('.extraItem').text();
+let item = document.querySelectorAll('.extraItem').textContent;
+const orderWrapper = $('.orderWrapper');
 const arr = [];
-
+let extraArr = [];
 $('.add').on('click', function () {
   let total = $('.total');
-  let selectedOption = $('option:selected').text().split('$').pop();
-  let number = parseFloat(selectedOption) * itemQuantity.val() * quantity.val();
+  let poppedOption = $('option:selected').text().split('$').pop();
+  let selectedOption = $('option:selected').text().split('+');
+  let number = parseFloat(poppedOption) * itemQuantity.val() * quantity.val();
   let addExtraItem = $(`<p class='extraItem' id='extraItem'><span class='x'></span></p>`);
   let x = 4.5 * quantity.val();
-
+  const optionToPush = `${itemQuantity.val()} Extra ${selectedOption[0]} +$${number}`;
   arr.push(number);
-  let price007 = arr.reduce((total, amount) => total + amount);
+  extraArr.push(optionToPush);
 
   extraItems.append(addExtraItem);
-  addExtraItem.prepend(`${itemQuantity.val()} Extra Tomatoes&nbsp;&nbsp;   +$ ${number.toFixed(2)}`);
+
+  addExtraItem.prepend(optionToPush);
+  let price007 = arr.reduce((total, amount) => total + amount);
   total.text('Total: $' + (x + price007).toFixed(2));
 
+  console.log(extraArr);
   $('span').click(function (e) {
     $(e.target).parent().hide(500);
   });
 });
+
 $('#placeOrder').click(() => {
-  $('.orderWrapper').append(`<div class="orderContainer">
-<p>
-  <span id="indicator">${itemQuantity.val()}</span>McDonald's Quarter 
-</p>
- <p class="orderContent">${extraItems.text()}</p>
-              </div>`);
+  let textToAppend = `
+    <div class="orderContainer">
+      <p>
+        <span id="indicator">${itemQuantity.val()}</span>McDonald's Quarter 
+      </p>
+      <p class='orderContent'>${$('#extraItem').text()}</p>
+    </div>
+    `;
+  orderWrapper.append(textToAppend);
 });
 
 $('.x').click(function (e) {
