58f2a23c6aa98e6ff7fd0f83d125d4e3e83ab41d
Ardonit Saliji <ardonit.1980@gmail.com>
2022-09-08 08:08:31 +0200

    better account verification

--- backend/routes.js
+++ backend/routes.js
@@ -1,35 +1,38 @@
-const express = require("express");
-const router = express.Router();
-const signUpModel = require("./signUpModels");
-router.post("/api/login", async (req, res) => {
-  const email = await signUpModel.find({
+const express = require('express')
+const router = express.Router()
+const signUpModel = require('./signUpModels')
+router.post('/api/login', async (req, res) => {
+  const email = await signUpModel.find({ emailOrPhone: req.body.emailOrPhone })
+  const account = await signUpModel.findOne({
     emailOrPhone: req.body.emailOrPhone,
-  });
-  const password = await signUpModel.find({ password: req.body.password });
-  if (email && email.length > 0 && password && password.length > 0) {
-    return res.status(200).send({ login: "successful" });
+    password: req.body.password,
+  })
+  if (account) {
+    return res.status(200).send({ login: 'successful' })
   }
-  if (email.length < 1) {
-    return res.status(404).send({ login: "Account does not exist" });
+  if (email.length < 1 && !account) {
+    return res.status(404).send({ login: 'Account does not exist' })
   }
-  if (email && email.length > 0 && password.length < 1) {
-    return res.status(401).send({ login: "wrong password" });
+  if (email && !account) {
+    return res.status(401).send({ login: 'wrong password' })
   }
-});
+})
 
-router.post("/api/signup", async (req, res) => {
-  const userExists = await signUpModel.findOne({
-    emailOrPhone: req.body.emailOrPhone,
-  });
+router.post('/api/signup', async (req, res) => {
+  const userExists = await signUpModel
+    .find({
+      emailOrPhone: req.body.emailOrPhone,
+    })
+    .limit(1)
   if (userExists && userExists !== []) {
-    return res.status(409).json({ error: "User already exists" });
+    return res.status(409).json({ error: 'User already exists' })
   }
   const user = new signUpModel({
     emailOrPhone: req.body.emailOrPhone,
     password: req.body.password,
-  });
-  const userCreated = await user.save();
-  return res.status(201).json({ data: { id: userCreated.id } });
-});
+  })
+  const userCreated = await user.save()
+  return res.status(201).json({ data: { id: userCreated.id } })
+})
 
-module.exports = router;
+module.exports = router
--- src/components/Popups/Account/Account.jsx
+++ src/components/Popups/Account/Account.jsx
@@ -1,36 +1,32 @@
-import React from "react";
-import "./Account.css";
+import React from 'react'
+import './Account.css'
 const Account = ({ setLoginStatus, accountPopup, setAccountPopup }) => {
   return (
     <div
-      style={
-        accountPopup
-          ? { transform: "translateX(0)" }
-          : { transform: "translateX(100%)" }
-      }
-      className="account-popup same"
+      style={accountPopup ? { transform: 'translateX(0)' } : { transform: 'translateX(100%)' }}
+      className='account-popup same'
     >
-      <div className="account-credentials">
-        <div className="account-username">
+      <div className='account-credentials'>
+        <div className='account-username'>
           <h3>Ardonit Saliji</h3>
           <p>@Ardonit</p>
         </div>
         <img
           onClick={() => setAccountPopup(false)}
-          src="https://yt3.ggpht.com/yti/AJo0G0m-2Y8iR9ecaHPVTitXtXBSyBivDb6SNOPmBg=s88-c-k-c0x00ffffff-no-rj-mo"
-          alt=""
+          src='https://yt3.ggpht.com/yti/AJo0G0m-2Y8iR9ecaHPVTitXtXBSyBivDb6SNOPmBg=s88-c-k-c0x00ffffff-no-rj-mo'
+          alt=''
         />
       </div>
-      <div className="account-lists">
+      <div className='account-lists'>
         <ul>
           <li></li>
           <li></li>
           <li></li>
           <li
             onClick={() => {
-              sessionStorage.setItem("loginStatus", JSON.stringify(false));
-              setAccountPopup(false);
-              setLoginStatus(false);
+              sessionStorage.setItem('loginStatus', JSON.stringify(false))
+              setAccountPopup(false)
+              setLoginStatus(false)
             }}
           >
             Logout
@@ -38,7 +34,7 @@ const Account = ({ setLoginStatus, accountPopup, setAccountPopup }) => {
         </ul>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Account;
+export default Account
