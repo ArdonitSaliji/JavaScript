684b21f604f670f053ccf42603c54f466f61a81b
Ardonit Saliji <ardonit.1980@gmail.com>
2022-11-03 00:14:35 +0100

    barbershop-project added

---
+++ barbershop-project/src/App.js
@@ -0,0 +1,73 @@
+import { useState, useEffect, useLayoutEffect } from 'react'
+import { nanoid } from 'nanoid'
+import NotesList from './components/NotesList'
+import Header from './components/Header'
+
+import Search from './components/Search'
+
+function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'These are my notes, so feel free to type some!',
+      date: '7/15/2022',
+    },
+  ])
+
+  const [searchText, setSearchText] = useState('')
+  const [darkMode, setDarkMode] = useState(true)
+  const savedNotes = localStorage.getItem('notes-data')
+  const savedMode = JSON.parse(localStorage.getItem('darkMode'))
+
+  useLayoutEffect(() => {
+    localStorage.setItem('notes-data', JSON.stringify(savedNotes))
+    setDarkMode(savedMode)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(darkMode))
+  }, [darkMode])
+
+  useEffect(() => {
+    localStorage.setItem('notes-data', JSON.stringify(notes))
+  }, [notes])
+
+  const addNote = (text) => {
+    const date = new Date()
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    }
+    const newNotes = [...notes, newNote]
+    setNotes(newNotes)
+  }
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id)
+    setNotes(newNotes)
+  }
+
+  return (
+    <div className={darkMode ? 'dark-mode' : 'light-mode'}>
+      <div className='container'>
+        <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Search
+          notes={notes}
+          setNotes={setNotes}
+          darkMode={darkMode}
+          handleSearchNote={setSearchText}
+        />
+
+        <NotesList
+          setNotes={setNotes}
+          notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))}
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
